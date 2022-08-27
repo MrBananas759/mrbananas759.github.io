0b0d05ad29c7dca03c77780932ae8d1933c15d0a
MrBananas759 <halo_2_372@hotmail.com>
2022-08-27 16:43:42 +1000

    as

--- BananaBot.js
+++ BananaBot.js
@@ -6,8 +6,8 @@ if (autoClick) {
 	Game.ClickCookie();
 }
 
-Document.addEventListener('keydown', function(event)) {
+Document.addEventListener('keydown', function(event) {
 	if(event.keyCode == 49) {
 		autoClick == !autoClick;
 	}
-}
+});
